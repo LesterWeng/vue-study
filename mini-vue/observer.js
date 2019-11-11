0b92bba3e441b19b8a95267cb7c01efa418a0129
wenglx <978656353@qq.com>
2019-11-11 18:07:13 +0800

    update

--- mini-vue/observer.js
+++ mini-vue/observer.js
@@ -1,59 +1,74 @@
 class Dep {
+  static target = null;
+
   constructor() {
     this.value = null;
-    this.obs = [];
+    this.subs = [];
   }
 
-  addOb(ob) {
-    const index = this.obs.indexOf(ob);
+  // 由于要维护sub的depIds数据，只能由sub(watcher)主动增删
+  addSub(sub) {
+    const index = this.subs.indexOf(sub);
     if (index === -1) {
-      this.obs.push(ob);
+      this.subs.push(sub);
     }
   }
 
-  removeOb(ob) {
-    const index = this.obs.indexOf(ob);
+  remSub(sub) {
+    const index = this.subs.indexOf(sub);
     if (index !== -1) {
-      this.obs.splice(index, 1);
+      this.subs.splice(index, 1);
     }
+  }
+
+  depend() {
+    this.addSub(Dep.target);
   }
 
   notify() {
-    for (const ob of this.obs) {
-      ob.update(this.value);
+    for (const sub of this.subs) {
+      sub.update(this.value);
     }
   }
 }
 
 export class Observer {
-  defineReactive(data) {
-    for (const k of Object.keys(data)) {
-      const val = data[k];
-      const dep = new Dep();
+  constructor(data) {
+    this.walk(data);
+  }
 
-      if (Object.prototype.toString.call(val) === '[object Object]') {
-        observe(val);
-      } else {
-        Object.defineProperty(data, k, {
-          configurable: true,
-          enumerable: true,
-          get() {
-            // 这里数据可能用于N个元素的渲染
-            const watcher = new Watcher('.div' + k.toUpperCase());
-            dep.addOb(watcher);
-            return val;
-          },
-          set(newVal) {
-            if (val === newVal) return;
+  walk(data) {
+    for (const key of Object.keys(data)) {
+      const val = data[key];
+      this.defineReactive(data, key, val);
+    }
+  }
 
-            val = newVal;
-            dep.value = newVal;
-            dep.notify();
-          },
-        });
-      }
+  defineReactive(data, key, val) {
+    const dep = new Dep();
+
+    if (Object.prototype.toString.call(val) === '[object Object]') {
+      observe(val);
+    } else {
+      Object.defineProperty(data, key, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          dep.depend();
+          return val;
+        },
+        set(newVal) {
+          if (val === newVal) return;
+
+          val = newVal;
+          dep.value = newVal;
+          dep.notify();
+        },
+      });
     }
   }
 }
 
-export function observe() {}
+export function observe(data) {
+  return new Observer(data);
+}
