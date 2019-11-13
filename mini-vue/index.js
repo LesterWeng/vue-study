b0d69afeaeed012cf38221c27aeff10a684c910c
wenglx <978656353@qq.com>
2019-11-13 11:32:48 +0800

    update

--- mini-vue/index.js
+++ mini-vue/index.js
@@ -1,9 +1,3 @@
-// 观察者模式-数据驱动Dom
-// 关系:一数据 -> 多dom
-
-// 数据 => data中的key => 抽象为依赖Dep
-// Dom(异步调度-vDom-render) <= 抽象的观察者Watcher
-
 import { observe } from "./observer";
 import { Compiler } from "./compiler";
 
